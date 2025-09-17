7e37d32b47082a4e06de84d7bfc337cc1416de3c
ravindu421 <ravindu.balasooriya@layoutindex.lk>
2025-09-17 18:47:00 +0530

    corrected database link

--- application/config/database.php
+++ application/config/database.php
@@ -6,14 +6,14 @@ $query_builder = TRUE;
 
 $db['default'] = array(
   'dsn'      => '',
-  'hostname' => 'db',       // service name from docker-compose.yml
+  'hostname' => 'host.docker.internal',       // service name from docker-compose.yml
   'username' => 'root',
   'password' => 'root',     // must match MYSQL_ROOT_PASSWORD
-  'database' => 'ci3app',
+  'database' => 'cinesync',
   'dbdriver' => 'mysqli',
   'pconnect' => FALSE,
   'db_debug' => (ENVIRONMENT !== 'production'),
-  'char_set' => 'utf8',
+  'char_set' => 'utf8mb4',
   'dbcollat' => 'utf8_general_ci',
   'save_queries' => TRUE,
   'port'     => 3306        // internal MySQL port
--- application/logs/log-2025-09-17.php
+++ application/logs/log-2025-09-17.php
@@ -51,3 +51,38 @@ AND `active` = 1
 ERROR - 2025-09-17 10:28:53 --> 404 Page Not Found: Api/docs
 ERROR - 2025-09-17 10:28:54 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_404.php): Failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 183
 ERROR - 2025-09-17 10:28:54 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_404.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 11:11:36 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'cinesync' /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 211
+ERROR - 2025-09-17 11:11:36 --> Unable to connect to the database
+ERROR - 2025-09-17 11:11:36 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/system/core/Exceptions.php:272) /var/www/html/system/core/Common.php 571
+ERROR - 2025-09-17 11:18:50 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'cinesync' /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 211
+ERROR - 2025-09-17 11:18:50 --> Unable to connect to the database
+ERROR - 2025-09-17 11:18:50 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/system/core/Exceptions.php:272) /var/www/html/system/core/Common.php 571
+ERROR - 2025-09-17 11:18:51 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'cinesync' /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 211
+ERROR - 2025-09-17 11:18:51 --> Unable to connect to the database
+ERROR - 2025-09-17 11:18:51 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/system/core/Exceptions.php:272) /var/www/html/system/core/Common.php 571
+ERROR - 2025-09-17 11:18:52 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'cinesync' /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 211
+ERROR - 2025-09-17 11:18:52 --> Unable to connect to the database
+ERROR - 2025-09-17 11:18:52 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/system/core/Exceptions.php:272) /var/www/html/system/core/Common.php 571
+ERROR - 2025-09-17 11:19:07 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'cinesync' /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 211
+ERROR - 2025-09-17 11:19:07 --> Unable to connect to the database
+ERROR - 2025-09-17 11:19:07 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/system/core/Exceptions.php:272) /var/www/html/system/core/Common.php 571
+ERROR - 2025-09-17 11:19:08 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'cinesync' /var/www/html/system/database/drivers/mysqli/mysqli_driver.php 211
+ERROR - 2025-09-17 11:19:08 --> Unable to connect to the database
+ERROR - 2025-09-17 11:19:08 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/system/core/Exceptions.php:272) /var/www/html/system/core/Common.php 571
+ERROR - 2025-09-17 11:41:25 --> Query error: Table 'cinesync.doc_mappings' doesn't exist - Invalid query: SELECT `wp_slug`, `wp_cpt`
+FROM `doc_mappings`
+WHERE `controller` = 'concession'
+AND `method` = 'new_form'
+AND `active` = 1
+ERROR - 2025-09-17 11:43:29 --> 404 Page Not Found: Api/docs
+ERROR - 2025-09-17 11:43:29 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_404.php): Failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 11:43:29 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_404.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 11:49:35 --> 404 Page Not Found: Api/docs
+ERROR - 2025-09-17 11:49:35 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_404.php): Failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 11:49:35 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_404.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 12:24:25 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-09-17 12:24:25 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_404.php): Failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 12:24:25 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_404.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 12:42:07 --> 404 Page Not Found: Faviconico/index
+ERROR - 2025-09-17 12:42:07 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_404.php): Failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 183
+ERROR - 2025-09-17 12:42:07 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_404.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 183
